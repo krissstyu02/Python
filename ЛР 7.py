10e7bc718f4b0350cb563b477b4c90988bae6155
Кристина <kristi090217@gmail.com>
2022-09-14 21:04:35 +0300

    загружена ЛР 7

---
+++ ЛР 7.py
@@ -0,0 +1,8 @@
+#ЛР7
+input('Введите рост учеников')
+a = [int(s) for s in input().split()]
+x = int(input('Введите рост Пети='))
+num = 0
+while num < len(a) and a[num] >= x:
+    num+=1
+print(num + 1)
