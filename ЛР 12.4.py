4d0c04ec580b94a552c4defde7ae6afdf7cbd4cc
Кристина <kristi090217@gmail.com>
2022-09-29 15:15:38 +0300

    добавлена 12.4

---
+++ ЛР 12.4.py
@@ -0,0 +1,16 @@
+
+
+s=input('Введите имя исходного файла=')
+s1=input('Введите имя целевого файла=')
+outf = open(s1 + '.txt', "w")
+
+
+file = open( s + '.txt', "r")
+lines = file.readlines()
+k=0
+for line in lines:
+    k+=1
+    w=str(k) + ': '+line
+    outf.write(w)
+
+
